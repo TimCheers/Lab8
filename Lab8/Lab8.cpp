878b8c4f54c11dc9ccdd6a6f0a69c97fb15a7b5e
TimCheers <73362594+TimCheers@users.noreply.github.com>
2021-01-31 12:47:01 +0500

    кастыль

--- Lab8/Lab8.cpp
+++ Lab8/Lab8.cpp
@@ -2,11 +2,6 @@
 #include<string>
 using namespace std;
 
-//dat1 - день
-//dat2 - месяц
-//dat3 - год
-
-
 struct Student
 {
 	void Show()
@@ -18,18 +13,11 @@ struct Student
 };
 
 
-
-int main()
+void CorNumb(Student*& st, int N, int k)
 {
 	system("chcp 1251>nul");
-	int N = -1,dat01=-1,dat02=-1,dat03=-1,reiting1=-1;
-	while (N<=0)
-	{
-		cout << "Введите количество студентов";
-		cin >> N;
-	}
-	Student *st=new Student[N];
-	for (int i = 0; i < N; i++)
+	int  dat01 = -1, dat02 = -1, dat03 = -1, reiting1 = -1;
+	for (int i = k; i < N; i++)
 	{
 		cout << "Введите ФИО:\t";
 		cin >> st[i].famil; getline(cin, st[i].name);
@@ -64,6 +52,58 @@ int main()
 		dat03 = -1;
 		reiting1 = -1;
 	}
+}
+void CorNumb2(Student*& st2, int N, int k)
+{
+	system("chcp 1251>nul");
+	int  dat01 = -1, dat02 = -1, dat03 = -1, reiting1 = -1;
+	for (int i = k; i < N; i++)
+	{
+		cout << "Введите ФИО:\t";
+		cin >> st2[i].famil; getline(cin, st2[i].name);
+		cout << "Введите адрес:\t";
+		getline(cin, st2[i].adres);
+		cout << "Введите дату рождения:\n";
+		while ((dat01 <= 0 || dat01 >= 30) && (dat02 <= 0 || dat02 >= 12) && (dat03 <= 0 || dat03 < 1950))
+		{
+			cout << "День:\t"; cin >> dat01;
+			cout << "Месяц:\t"; cin >> dat02;
+			cout << "Год:\t"; cin >> dat03;
+			if ((dat01 <= 0 || dat01 > 30) && (dat02 <= 0 || dat02 >= 12) && (dat03 <= 0 || dat03 < 1950))
+			{
+				cout << "Введены некоректные данные! Введите дату рождения заново!" << endl;
+			}
+			st2[i].dat1 = dat01;
+			st2[i].dat2 = dat02;
+			st2[i].dat3 = dat03;
+		}
+		while (st2[i].reiting < 0)
+		{
+			cout << "Введите рейтинг:\t";
+			cin >> reiting1;
+			if (reiting1 < 0)
+			{
+				cout << "Введены некоректные данные!" << endl;
+			}
+			st2[i].reiting = reiting1;
+		}
+		dat01 = -1;
+		dat02 = -1;
+		dat03 = -1;
+		reiting1 = -1;
+	}
+}
+int main()
+{
+	system("chcp 1251>nul");
+	int N = -1;
+	while (N <= 0)
+	{
+		cout << "Введите количество студентов";
+		cin >> N;
+	}
+	Student* st = new Student[N];
+	CorNumb(st, N, 0);
 	/////////////////////////УДАЛЕНИЕ///////////////////////////////////
 	for (int i = 0; i < N; i++)
 	{
@@ -85,9 +125,9 @@ int main()
 			}
 			if (day==true && month==true && ear==true)
 			{
-				for (int l = j; l <= N; l++)
+				f++;
+				for (int l = j; l < N-1; l++)
 				{
-					f++;
 					st[l].dat1 = st[l + 1].dat1;
 					st[l].dat2 = st[l + 1].dat2;
 					st[l].dat3 = st[l + 1].dat3;
@@ -101,7 +141,7 @@ int main()
 		}
 		if (f>0)
 		{
-			for (int l = i; l <= N; l++)
+			for (int l = i; l < N-1; l++)
 			{
 				st[l].dat1 = st[l + 1].dat1;
 				st[l].dat2 = st[l + 1].dat2;
@@ -116,32 +156,41 @@ int main()
 	}
 
 	////////////////////////////ДОБАВЛЕНИЕ///////////////////////////////////////
-	//string Sname;
-	//cout << "Введите фамилию нужную";
-	//cin >> Sname;
-	//Student *st2=new Student[N+1];
-	//Student Obj;
-	//for (int i = 0; i < N; i++)
-	//{
-	//	if (st[i].getFam()==Sname)
-	//	{
-	//		for (int j = 0; j < i; j++)
-	//		{
-	//			st2[j] = st[i];
-	//		}
-	//		st2[i] = st[i];//////////////////////////////
-	//		for (int j = i+1; j < N; j++)
-	//		{
-	//			st2[j] = st[j - 1];
-	//		}
-	//	}
-	//}
-	///////////////////////////////////////////////////////////////////////////////
-
-	cout << endl << endl;
+	string Sname;
+	cout << "Введите фамилию нужную";
+	cin >> Sname;
+	Student *st2=new Student[N+1];
+	Student Obj;
+	cout << "Введите параметры нвого чела" << endl;
 	for (int i = 0; i < N; i++)
 	{
-		st[i].Show();
+		if (st[i].famil==Sname)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				st2[j].dat1 = st[j].dat1;
+				st2[j].dat2 = st[j].dat2;
+				st2[j].dat3 = st[j].dat3;
+				st2[j].adres = st[j].adres;
+				st2[j].famil = st[j].famil;
+				st2[j].name = st[j].name;
+				st2[j].reiting = st[j].reiting;
+			}
+			CorNumb2(st2, i+1, i);
+			for (int j = i+1; j < N+1; j++)
+			{
+				st2[j] = st[j - 1];
+			}
+			i = N;
+		}
+	}
+	///////////////////////////////////////////////////////////////////////////////
+
+	
+	cout << endl << endl;
+	for (int i = 0; i < N+1; i++)
+	{
+		st2[i].Show();
 	}
 	return 0;
 }
